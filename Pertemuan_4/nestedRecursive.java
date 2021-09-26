743390381479c36d43bbbfbb1656757bc9e7edb9
M.Wildan Fadhilah <70873318+wildanfhd@users.noreply.github.com>
2021-09-26 17:34:49 +0700

    Add files via upload

---
+++ Pertemuan_4/nestedRecursive.java
@@ -0,0 +1,24 @@
+package com.program;
+
+import java.util.Scanner;
+//import java.lang.String;
+
+public class nestedRecursive {
+    public static void main(String[] args) {
+        Scanner ourInput = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int nilai = ourInput.nextInt();
+        int nilai_fibonacci = fibonacci(nilai, "atas");
+        System.out.println("nilai fibonacci ke-" + nilai + " adalah " + nilai_fibonacci);
+    }
+    
+    private static int fibonacci(int n, String daun) {
+        System.out.println("daun " + daun);
+        System.out.println("Fibonacci ke-" + n);
+        if(n == 1 || n == 0){
+            return n;
+        } else {
+            return fibonacci(n - 1, "kiri") + fibonacci(n - 2, "kanan");
+        }
+    }
+}
