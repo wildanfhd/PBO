d031a64250d7f79331c10747c14d7fac5370b723
M.Wildan Fadhilah <70873318+wildanfhd@users.noreply.github.com>
2021-10-29 19:50:10 +0700

    Create Manusia.java

---
+++ Pertemuan_9/Manusia.java
@@ -0,0 +1,33 @@
+abstract class Manusia {
+    protected String namaDepan;
+    protected String namaBelakang;
+    protected int umur;
+
+    public String getNamaDepan() {
+        return namaDepan;
+    }
+
+    public void setNamaDepan(String namaDepan) {
+        this.namaDepan = namaDepan;
+    }
+
+    public String getNamaBelakang() {
+        return namaBelakang;
+    }
+
+    public void setNamaBelakang(String namaBelakang) {
+        this.namaBelakang = namaBelakang;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    // Abstract Method
+    public abstract void dataSingkat();
+    public abstract void namaLengkap();
+}
